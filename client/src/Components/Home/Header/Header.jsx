0fb4218e792f278c0ba4ae0c4e3c4d3a9a6e5ec9
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-10 22:26:33 +0700

    skeleton for header

--- client/src/Components/Home/Header/Header.jsx
+++ client/src/Components/Home/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import SearchHeader from "./SearchHeader/SearchHeader";
 import style from "./Header.module.css";
 import {
@@ -9,7 +9,7 @@ import {
   CarOutlined,
   EnvironmentOutlined,
 } from "@ant-design/icons";
-import { Button, Popover, Menu } from "antd";
+import { Button, Popover, Menu, Skeleton } from "antd";
 import { ThemeContext } from "../../../App";
 import ModalHeader from "./ModalHeader/ModalHeader";
 import { useTranslation } from "react-i18next";
@@ -23,6 +23,11 @@ export default function Header() {
   );
   const [selectedCurrency, setSelectedCurrency] = useState("USD");
   const [selectedCountry, setSelectedCountry] = useState("United States");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -38,7 +43,6 @@ export default function Header() {
   const handleLanguageChange = (value) => {
     setSelectedLanguage(value);
     i18n.changeLanguage(value);
-
   };
 
   const handleCurrencyChange = (value) => {
@@ -81,64 +85,71 @@ export default function Header() {
     <>
       <div id={style[theme]} className={style.header_block}>
         <div className={style.main_block}>
-          <div className={style.main_center_block}>
-            <div className={style.left_block}>
-              <div className={style.logo}>
-                <img
-                  className={style.img_logo}
-                  src="./img/logo/logo3.png"
-                  alt="logo_jetsearch"
-                />
-                <div className={style.text_logo}>JetSearch</div>
-              </div>
+          {isLoading ? (
+            <div className={style.skeleton}>
+              <Skeleton active paragraph={{ rows: 0 }} />
             </div>
-            <div className={style.right_block}>
-              <div className={style.icon_world}>
-                <Button
-                  onClick={showModal}
-                  id={style[theme]}
-                  className={style.world_button}
-                  type="primary"
-                  icon={<GlobalOutlined className={style.icon_bold} />}
-                />
+          ) : (
+            <div className={style.main_center_block}>
+              <div className={style.left_block}>
+                <div className={style.logo}>
+                  <img
+                    className={style.img_logo}
+                    src="./img/logo/logo3.png"
+                    alt="logo_jetsearch"
+                  />
+                  <div className={style.text_logo}>JetSearch</div>
+                </div>
               </div>
-              <div className={style.user_block}>
-                <Button
-                  id={style[theme]}
-                  className={style.user_button}
-                  icon={<UserOutlined className={style.icon_bold} />}
-                >
-                  {t("header.login")}
-                </Button>
-              </div>
-              <ModalHeader
-                isModalOpen={isModalOpen}
-                handleSave={handleSave}
-                handleCancel={handleCancel}
-                handleLanguageChange={handleLanguageChange}
-                handleCurrencyChange={handleCurrencyChange}
-                handleCountryChange={handleCountryChange}
-                selectedLanguage={selectedLanguage}
-                selectedCurrency={selectedCurrency}
-                selectedCountry={selectedCountry}
-              ></ModalHeader>
-              <div className={style.icon_menu}>
-                <Popover
-                  placement="bottomRight"
-                  content={menu}
-                  trigger="click"
-                  className={style.popover}
-                >
+              <div className={style.right_block}>
+                <div className={style.icon_world}>
+                  <Button
+                    onClick={showModal}
+                    id={style[theme]}
+                    className={style.world_button}
+                    type="primary"
+                    icon={<GlobalOutlined className={style.icon_bold} />}
+                  />
+                </div>
+                <div className={style.user_block}>
                   <Button
                     id={style[theme]}
-                    className={style.menu_button}
-                    type="primary"
-                    icon={<MenuOutlined className={style.icon_bold} />}
-                  />
-                </Popover>
+                    className={style.user_button}
+                    icon={<UserOutlined className={style.icon_bold} />}
+                  >
+                    {t("header.login")}
+                  </Button>
+                </div>
+                <ModalHeader
+                  isModalOpen={isModalOpen}
+                  handleSave={handleSave}
+                  handleCancel={handleCancel}
+                  handleLanguageChange={handleLanguageChange}
+                  handleCurrencyChange={handleCurrencyChange}
+                  handleCountryChange={handleCountryChange}
+                  selectedLanguage={selectedLanguage}
+                  selectedCurrency={selectedCurrency}
+                  selectedCountry={selectedCountry}
+                ></ModalHeader>
+                <div className={style.icon_menu}>
+                  <Popover
+                    placement="bottomRight"
+                    content={menu}
+                    trigger="click"
+                    className={style.popover}
+                  >
+                    <Button
+                      id={style[theme]}
+                      className={style.menu_button}
+                      type="primary"
+                      icon={<MenuOutlined className={style.icon_bold} />}
+                    />
+                  </Popover>
+                </div>
               </div>
             </div>
-          </div>
+          )}
+
           <SearchHeader />
         </div>
       </div>
