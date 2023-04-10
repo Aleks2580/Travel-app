4f6b3c4a73a49cf6dc5ba9c75a530311bc6fa17d
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-10 16:20:01 +0700

    skeleton for switch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import { createContext } from "react";
 import { useState, useEffect } from "react";
-import { Switch } from "antd";
+import { Switch, Skeleton } from "antd";
 import { useTranslation } from "react-i18next";
 import i18n from "i18next";
 
@@ -12,6 +12,7 @@ export const ThemeContext = createContext(null);
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [isLoading, setIsLoading] = useState(true);
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
@@ -25,18 +26,26 @@ function App() {
       setTheme(savedTheme);
     }
     i18n.changeLanguage(localStorage.getItem("language"));
+    setIsLoading(false);
   }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className={style.app_main} id={style[theme]}>
-        <Switch
-          onChange={toggleTheme}
-          className={style.switch}
-          checkedChildren={t("theme_switch.light")}
-          unCheckedChildren={t("theme_switch.dark")}
-          checked={theme === "dark"}
-        />
+        {isLoading ? (
+          <div className={style.skeleton}>
+            <Skeleton active size="large" paragraph={{ rows: 0 }} />
+          </div>
+        ) : (
+          <Switch
+            onChange={toggleTheme}
+            className={style.switch}
+            checkedChildren={t("theme_switch.light")}
+            unCheckedChildren={t("theme_switch.dark")}
+            checked={theme === "dark"}
+          />
+        )}
+
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
