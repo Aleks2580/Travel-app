c832f2952d9741cc0612929f6eba7f7ff977a6a9
Aleksei Vlasov <aleks101989@mail.ru>
2023-04-10 13:55:50 +0700

    theme saved in localstorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,28 @@ import style from "./App.module.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import { createContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Switch } from "antd";
 import { useTranslation } from "react-i18next";
 
 export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const toggleTheme = () => {
-    setTheme((current) => (current === "light" ? "dark" : "light"));
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
   const { t } = useTranslation();
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className={style.app_main} id={style[theme]}>
@@ -23,6 +33,7 @@ function App() {
           className={style.switch}
           checkedChildren={t("theme_switch.light")}
           unCheckedChildren={t("theme_switch.dark")}
+          checked={theme === "dark"}
         />
         <Routes>
           <Route path="/" element={<Home />} />
